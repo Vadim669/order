a0ffadef664f76c663e3eaba980b293232819875
Vadim669 <77455565+Vadim669@users.noreply.github.com>
2021-01-19 21:18:26 +0200

    change composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4501f89338ee94b0bde7ba0e7edf3802ac2573a7',
+    'name' => 'vadoz/shop',
+  ),
+  'versions' => 
+  array (
+    'vadoz/shop' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4501f89338ee94b0bde7ba0e7edf3802ac2573a7',
+    ),
+  ),
+);
